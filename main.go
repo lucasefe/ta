cdf42fb22ba06ee068cb21945c33954ee97956ae
Lucas Florio <lucasefe@gmail.com>
2016-07-29 09:20:29 -0700

    Dryrun mode

--- main.go
+++ main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"ta"
 )
 
-var tmux, config, session, defaultSession string
+var (
+	config         string
+	session        string
+	defaultSession string
+	dryrun         bool
+	tmux           string
+)
 
 func main() {
-	tmux = "/usr/local/bin/tmux"
 	defaultSession = path.Base(os.Getenv("PWD"))
+	tmux = "/usr/local/bin/tmux"
 	flag.StringVar(&config, "f", ".ta", "the ta config file")
-	flag.StringVar(&session, "s", defaultSession, "the ta config file")
+	flag.StringVar(&session, "s", defaultSession, "session name")
+	flag.BoolVar(&dryrun, "d", false, "dry run mode")
 	flag.Parse()
 
 	file, err := os.Open(config)
@@ -26,15 +35,26 @@ func main() {
 	defer file.Close()
 
 	for _, arguments := range ta.Parse(session, file) {
-		err := exec.Command(tmux, arguments...).Run()
+		err := runTmuxCommand(arguments)
 		if err != nil {
-			log.Println("args: %+v, err: %v", arguments, err)
+			log.Printf("args: %+v, err: %v\n", arguments, err)
 		}
 	}
 
-	cmd := exec.Command(tmux, ta.Args{"attach-session", "-t", session}...)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Run()
+	if !dryrun {
+		cmd := exec.Command(tmux, ta.Args{"attach-session", "-t", session}...)
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+}
+
+func runTmuxCommand(arguments ta.Args) error {
+	if dryrun {
+		fmt.Printf("%s %s\n", tmux, strings.Join(arguments, " "))
+		return nil
+	}
+
+	return exec.Command(tmux, arguments...).Run()
 }
